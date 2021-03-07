841b9c444b05f50f98d42f35db3342bb25f091ee
Aiden-Yoo <you1367@gmail.com>
2021-03-07 20:28:16 +0900

    FIX uploads: seperate path

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -14,37 +14,35 @@ import { editFileName, imageFileFilter } from './uploads.utils';
 
 @Controller('uploads')
 export class UploadsController {
-  @Post('')
-  @UseInterceptors(
-    FileInterceptor('file', {
-      storage: diskStorage({
-        destination: './uploads',
-        filename: editFileName,
-      }),
-      // fileFilter: imageFileFilter,
-    }),
-  )
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
-    const response = {
-      originalname: file.originalname,
-      filename: file.filename,
-    };
-    return response;
-  }
+  // @Post('')
+  // @UseInterceptors(
+  //   FileInterceptor('file', {
+  //     storage: diskStorage({
+  //       destination: './uploads',
+  //       filename: editFileName,
+  //     }),
+  //     // fileFilter: imageFileFilter,
+  //   }),
+  // )
+  // uploadFile(@UploadedFile() file: Express.Multer.File) {
+  //   console.log(file);
+  //   const response = {
+  //     originalname: file.originalname,
+  //     filename: file.filename,
+  //   };
+  //   return response;
+  // }
 
-  @Post('multiple')
+  @Post('firmwares')
   @UseInterceptors(
     FilesInterceptor('file', 10, {
       storage: diskStorage({
-        destination: './uploads',
+        destination: './uploads/firmwares',
         filename: editFileName,
       }),
-      // fileFilter: imageFileFilter,
     }),
   )
-  uploadMultipleFiles(@UploadedFiles() files: Express.Multer.File[]) {
-    console.log(files);
+  uploadFirmwareFiles(@UploadedFiles() files: Express.Multer.File[]) {
     const response = [];
     files.forEach(file => {
       const fileResponse = {
@@ -56,9 +54,62 @@ export class UploadsController {
     return response;
   }
 
-  @Get(':path')
-  seeUploadedFile(@Param('path') file, @Res() res) {
-    return res.sendFile(file, { root: './uploads' });
+  @Post('documents')
+  @UseInterceptors(
+    FilesInterceptor('file', 10, {
+      storage: diskStorage({
+        destination: './uploads/documents',
+        filename: editFileName,
+      }),
+    }),
+  )
+  uploadDocumentFiles(@UploadedFiles() files: Express.Multer.File[]) {
+    const response = [];
+    files.forEach(file => {
+      const fileResponse = {
+        originalname: file.originalname,
+        filename: file.filename,
+      };
+      response.push(fileResponse);
+    });
+    return response;
+  }
+
+  @Post('issues')
+  @UseInterceptors(
+    FilesInterceptor('file', 10, {
+      storage: diskStorage({
+        destination: './uploads/issues',
+        filename: editFileName,
+      }),
+      // fileFilter: imageFileFilter,
+    }),
+  )
+  uploadIssueFiles(@UploadedFiles() files: Express.Multer.File[]) {
+    const response = [];
+    files.forEach(file => {
+      const fileResponse = {
+        originalname: file.originalname,
+        filename: file.filename,
+      };
+      response.push(fileResponse);
+    });
+    return response;
+  }
+
+  @Get('firmwares/:path')
+  seeFirmwareFile(@Param('path') file, @Res() res) {
+    return res.sendFile(file, { root: './uploads/firmwares' });
+  }
+
+  @Get('documents/:path')
+  seeDocumentFile(@Param('path') file, @Res() res) {
+    return res.sendFile(file, { root: './uploads/documents' });
+  }
+
+  @Get('issues/:path')
+  seeIssueFile(@Param('path') file, @Res() res) {
+    return res.sendFile(file, { root: './uploads/issues' });
     // try {
     //   if (file.match(/\.(jpg|jpeg|png|gif)$/)) {
     //     return res.sendFile(file, { root: './uploads' });
